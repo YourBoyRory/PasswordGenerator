3e58d1b8b1eb81a2c32bdc4f9de83ed583742931
YourBoyRory <yourboyrory@gmail.com>
2024-07-25 12:05:55 -0400

    Checks word list for compliance

--- randPass.py
+++ randPass.py
@@ -1,15 +1,30 @@
 import random
 import os
 
+ # Configure for use case
+word_count=5           # amount of words to generate every time the user regens: 5
+filename = 'words.txt' # allowed word list file directory: words.txt
+
+ # Used to store the previously used words
+ # avoids same words when generating multiple words and makes regens feel more random
+used_words = ["0"]*word_count
+
+ # Return a random word form the allowed words list
 def generate_random_word(filename):
     with open(filename, 'r') as file:
         words = file.readlines()
-        return random.choice(words).strip()
+        return random.choice(words).strip() 
+        
+def clear_screen():
+    if os.name == "posix":  # If we are on Linux or MacOS
+        os.system('clear')
+    else:                   # If we are on Windows
+        os.system('cls')
+    print(" ")
 
-word_count=5
-used_words = ["0"]*word_count
-filename = 'words.txt'
-if not os.path.exists(filename):
+ # Creates a compliant demo allowed words list
+def make_compliant_wordlist(filename):
+     # demo words
     wordlist = [
         "Apple", "Banana", "Mango", "Papaya", "Peach", "Orange", 
         "Grape", "Kiwi", "Lemon", "Melon", "Pineapple", "Plum",
@@ -21,18 +36,45 @@ if not os.path.exists(filename):
         for word in wordlist:
             f.write(word+"\n")
 
-while True:
-    if os.name is "posix":
-        os.system('clear')
-    else:
-        os.system('cls')
-    print(" ")
-    for i in range(word_count):
-        random_word = generate_random_word(filename)
-        random_number = random.randint(1000, 9999)
-        while random_word in used_words:
-            random_word = generate_random_word(filename)
-        used_words[i] = random_word
-        print("   \033[37m\033[1m",random_word+"\033[36m"+str(random_number),"\033[0m")
-    print(" ")
-    input("Press Enter to regen...")
+ # Returns true or false if the word list has enough words to allow no repeats for specified word_count
+def check_wordlist_compliance(filename):
+    compliant_words = ["0"]*int(word_count+1)
+    try:
+        with open(filename, 'r') as file:
+            words = file.readlines()
+    except:
+        print("[\033[33mWARN\033[0m] Word list was missing, generated new one.","\n")
+        make_compliant_wordlist(filename)
+        return
+        
+    count=0
+    for word in words:
+        if word not in compliant_words:
+            compliant_words[count] = words[count]
+            count=count+1
+        if count is word_count+1:
+            break
+    if "0" in compliant_words:
+        print("[\033[33mWARN\033[0m] The wordlist was not compliant, generated new one.","\n")
+        make_compliant_wordlist(filename)
+        return
+    
+
+def main():
+    clear_screen()
+    check_wordlist_compliance(filename)
+    while True:
+        for i in range(word_count):
+            random_number = random.randint(1000, 9999)
+            while True: # Loop until we find a words that isnt in the used_words list
+                check_wordlist_compliance(filename)
+                random_word = generate_random_word(filename)
+                if random_word not in used_words:
+                    break
+            used_words[i] = random_word
+            print("   \033[37m\033[1m",random_word+"\033[36m"+str(random_number),"\033[0m")
+        input("\nPress Enter to regen...")
+        clear_screen()
+
+if __name__ == "__main__":
+    main()
