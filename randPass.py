4b765d2689019281505ecc071caae15523176ed6
YourBoyRor <yourboyrory@gmail.com>
2024-07-29 15:59:13 -0400

    Removed need for word list entirely

--- randPass.py
+++ randPass.py
@@ -2,17 +2,15 @@ import random
 import os
 
  # Configure for use case
-word_count=5           # amount of words to generate every time the user regens: 5
-number_mask="9"*4
-filename = 'words.txt' # allowed word list file directory: words.txt
-prefix=""
-subfix=""
+keyphrases=5               # amount of keyphrases to generate every time the user regens: 5
+number_mask="0"*2          # Mask for the number generated. "0"*4 - 4 didgets 0-9, "1000" first diget is 1-9 last 3 are 0-9: "0"*4
+filename ="words.txt"      # allowed word list file directory: words.txt
+word_amount=1              # amount of words to be selected from the allowed word list: 1
+word_divider=""            # divider that seperates each word in the keyphrase: ""
+sufix_selection="!@#$%^&"  # Selects a random char from this string to sufix the keyphrase, use this if you need a special character "!@#$%^&".: " "
+wordlist_required=True     # Do not use the default wordlist
 
- # Return a random word form the allowed words list
-def generate_wordlist(filename):
-    with open(filename, 'r') as file:
-        wordlist = file.readlines()
-        return wordlist
+
         
 def clear_screen():
     if os.name == "posix":  # If we are on Linux or MacOS
@@ -21,32 +19,29 @@ def clear_screen():
         os.system('cls')
     print(" ")
 
- # Creates a compliant demo allowed words list
-def make_compliant_wordlist(filename):
-     # demo words
-    wordlist = [
+ # Return complaint wordlist
+def generate_wordlist(filename):
+     # Used to store at least keyphrases+1 unique words
+    compliant_words = ["0"]*int(keyphrases+1)
+    default_wordlist = [
         "Apple", "Banana", "Mango", "Papaya", "Peach", "Orange", 
         "Grape", "Kiwi", "Lemon", "Melon", "Pineapple", "Plum",
         "Cherry", "Apricot", "Avocado", "Coconut", "Date", "Pomegranate",
         "Guava", "Lychee", "Nectarine", "Olive", "Pear", "Raspberry",
         "Strawberry"
     ]
-    with open(filename, 'w') as f:
-        for word in wordlist:
-            f.write(word+"\n")
-    print("[ \033[36mINFO\033[0m ] Generated new wordlist.","\n")
-
- # Returns true or false if the word list has enough words to allow no repeats for specified word_count
-def check_wordlist_compliance(filename):
-     # Used to store at least word_count+1 unique words
-    compliant_words = ["0"]*int(word_count+1)
     
-     # if the word list is missing, make a new one
+     # if the word list is missing, use default
     try:
-        words = generate_wordlist(filename)
+        with open(filename, 'r') as file:
+            words = file.readlines()
     except:
-        make_compliant_wordlist(filename)
-        return
+        if wordlist_required:
+            input("[ \033[31mFATAL\033[0m ] Wordlist is Required!")
+            exit()
+        else:
+            print("[ \033[33mWARN\033[0m ] Using default wordlist\n")
+            return default_wordlist
     
      # loops though the word list until it finds enough unique words
     count=0
@@ -54,15 +49,20 @@ def check_wordlist_compliance(filename):
         if word not in compliant_words:
             compliant_words[count] = words[count]
             count=count+1
-        if count is word_count+1:
+        if count is keyphrases+1:
             break
-    # if count is not equal to word_count+1 then we looped the entire array without finding enough unique words
-    if count is not word_count+1:
-        print("[ \033[33mWARN\033[0m ] The wordlist is too short!")
-        make_compliant_wordlist(filename)
-        return
+    # if count is not equal to keyphrases+1 then we looped the entire array without finding enough unique words
+    if count is not keyphrases+1:
+        print("[ \033[31mEROR\033[0m ] Wordlist is too short!")
+        if wordlist_required:
+            input("[ \033[31mFATL\033[0m ] Wordlist is Required!")
+            exit()
+        else:
+            print("[ \033[33mWARN\033[0m ] Using default wordlist\n")
+            return default_wordlist
+    return words
 
-def format_word(random_word):
+def format_keyphrase(random_word):
     new_random_word = ""
     for char in random_word:
         if char.isupper():
@@ -78,26 +78,29 @@ def format_word(random_word):
 def generate_pin_number(mask):
     pin=""
     for char in mask:
-        pin += str(random.randint(0, int(char)))
+        pin += str(random.randint(int(char), 9))
     return pin
 
 def main():
      # Used to store the previously used words
      # avoids same words when generating multiple words and makes regens feel more random
-    used_words = ["0"]*word_count
+    used_words = ["0"]*keyphrases
     
     clear_screen()
-    check_wordlist_compliance(filename)
     wordlist=generate_wordlist(filename)
     while True:
-        for i in range(word_count):
-            while True: # Loop until we find a words that isnt in the used_words list
-                random_word = random.choice(wordlist).strip() 
-                if random_word not in used_words:
-                    break
+        for i in range(keyphrases):
+            random_word=""
+            for j in range(word_amount):
+                while True: # Loop until we find a words that isnt in the used_words list
+                    temp_word = random.choice(wordlist).strip() 
+                    if random_word+temp_word not in used_words and temp_word not in random_word:
+                        random_word += temp_word
+                        if j+1 is not word_amount:
+                            random_word += word_divider
+                        break
             used_words[i] = random_word
-            format_word(random_word)
-            print("   \033[32m[",i+1,"]\033[0m", format_word(prefix+random_word+generate_pin_number(number_mask)+subfix))
+            print("   \033[32m[",i+1,"]\033[0m", format_keyphrase(random_word+generate_pin_number(number_mask)+random.choice(sufix_selection)))
         input("\nPress Enter to regenerate...")
         clear_screen()
 
